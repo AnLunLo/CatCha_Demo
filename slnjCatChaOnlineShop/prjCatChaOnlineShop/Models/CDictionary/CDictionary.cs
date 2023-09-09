6e2ac132cc016d25f017a6128abf5e0112385680
Allun <ocarina51006@gmail.com>
2023-09-09 21:35:48 +0800

    DEMO連線版上傳

---
+++ slnjCatChaOnlineShop/prjCatChaOnlineShop/Models/CDictionary/CDictionary.cs
@@ -0,0 +1,18 @@
+﻿namespace prjCatChaOnlineShop.Models.CDictionary
+{
+    public class CDictionary
+    {
+        //拿來當作KEY的名稱放在這邊，用點選的才不會打錯
+        //變數名稱就等於內容值
+
+        public static readonly string SK_PURCHASED_PRODUCTS_LIST = "SK_PURCHASED_PRODUCTS_LIST";
+
+        public static readonly string SK_LOINGED_USER = "SK_LOINGED_USER";
+
+        public static readonly string SK_PAY_MODEL = "SK_PAY_MODEL";
+
+        public static readonly string SK_PAYMEMENT_MODEL = "SK_PAYMEMENT_MODEL";
+
+        public static readonly string SK_FINALTOTALPRICE = "SK_FinalTotalPrice";
+    }
+}
